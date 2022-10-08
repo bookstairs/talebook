ca9abac3f29d2dbd4ce85b6f839717e67d0dd3a3
licong <licong@qianxin.com>
2022-10-08 15:38:58 +0800

    fix talebook startup error on linux

--- config/calibre_linux.go
+++ config/calibre_linux.go
@@ -1,6 +1,29 @@
 package config
 
-const (
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
+var (
 	defaultCalibreDB = "/opt/calibre/bin/calibredb"
 	defaultConvert   = "/opt/calibre/bin/ebook-convert"
 )
+
+func init() {
+	if _, err := os.Stat(defaultCalibreDB); errors.Is(err, os.ErrNotExist) {
+		calibreDb, err := exec.LookPath("calibredb")
+		if err == nil {
+			defaultCalibreDB = calibreDb
+		}
+	}
+
+	if _, err := os.Stat(defaultConvert); errors.Is(err, os.ErrNotExist) {
+		ebookConvert, err := exec.LookPath("ebook-convert")
+		if err == nil {
+			defaultConvert = ebookConvert
+		}
+	}
+
+}
